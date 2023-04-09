dff134f5c8ff84fbe6d0e6723cafdb4dab3e9a38
mgckaled <marcelgckaled@gmail.com>
2023-04-09 14:57:09 -0300

    feat(d01/web): upload basic react project

---
+++ d01/web/src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Home } from './pages/Home/Index'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App () {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
+  )
+}
