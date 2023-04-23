17ef34aa25db418c1f91c2b76d713beb59d53610
mgckaled <marcelgckaled@gmail.com>
2023-04-23 13:35:09 -0300

    feat(d05): improve calculator logic and add opacity when  active buttons

--- d05/template/script.js
+++ d05/template/script.js
@@ -6,6 +6,12 @@ const resultDisplay = document.getElementById('result-display')
 let inputBuffer = ''
 
 function appendInput (input) {
+  if (
+    inputBuffer === '' &&
+    (input === '*' || input === '/' || input === '+' || input === '-')
+  ) {
+    return
+  }
   inputBuffer += input
   if (input === '/') {
     operationDisplay.value += ' ÷ '
@@ -16,22 +22,47 @@ function appendInput (input) {
   } else if (input === '-') {
     operationDisplay.value += ' - '
   } else if (input === '%') {
-    operationDisplay.value += ' ' + input + ' '
+    if (inputBuffer.length === 1) {
+      operationDisplay.value = ''
+    } else {
+      operationDisplay.value += ' ' + input + ' '
+    }
   } else {
+    if (
+      operationDisplay.value.slice(-1) === '+' ||
+      operationDisplay.value.slice(-1) === '-' ||
+      operationDisplay.value.slice(-1) === 'x' ||
+      operationDisplay.value.slice(-1) === ' ÷ '
+    ) {
+      operationDisplay.value = operationDisplay.value.slice(0, -1)
+    }
     operationDisplay.value += input
   }
+  resultDisplay.value = ''
 }
 
 function calculateResult () {
+  if (
+    inputBuffer === '' ||
+    inputBuffer.slice(-1) === '+' ||
+    inputBuffer.slice(-1) === '-' ||
+    inputBuffer.slice(-1) === '*' ||
+    inputBuffer.slice(-1) === '/'
+  ) {
+    return
+  }
   try {
     const result = eval(inputBuffer)
     if (Number.isInteger(result)) {
-      resultDisplay.value = result
+      resultDisplay.value = numberWithCommas(result)
     } else {
-      resultDisplay.value = result.toFixed(2).replace(/\.?0+$/, '')
+      resultDisplay.value = numberWithCommas(result.toFixed(2).replace(/\.?0+$/, ''))
     }
   } catch (error) {
     resultDisplay.value = 'Error'
+    console.log(error)
+    inputBuffer = ''
+    operationDisplay.value = ''
   }
 }
 
@@ -42,20 +73,58 @@ function clearAll () {
 }
 
 function clearEntry () {
-  inputBuffer = ''
-  operationDisplay.value = ''
+  if (inputBuffer.slice(-1) === ' ') {
+    operationDisplay.value = operationDisplay.value.slice(0, -2)
+  }
+  inputBuffer = inputBuffer.slice(0, -1)
+  resultDisplay.value = ''
 }
 
 function toggleSign () {
-  if (inputBuffer.charAt(0) === '-') {
-    inputBuffer = inputBuffer.substring(1)
+  if (
+    inputBuffer.slice(-1) === '+' ||
+    inputBuffer.slice(-1) === '-' ||
+    inputBuffer.slice(-1) === '*' ||
+    inputBuffer.slice(-1) === '/'
+  ) {
+    return
+  }
+  if (inputBuffer === '') {
+    return
+  }
+  if (inputBuffer.slice(0, 1) === '-') {
+    inputBuffer = inputBuffer.slice(1)
+    operationDisplay.value = operationDisplay.value.slice(1)
   } else {
     inputBuffer = '-' + inputBuffer
+    operationDisplay.value = '-' + operationDisplay.value
   }
-  operationDisplay.value = inputBuffer
+  resultDisplay.value = ''
 }
 
 function calculatePercentage () {
-  resultDisplay.value = eval(inputBuffer) / 100
-  operationDisplay.value += ' % '
+  if (
+    inputBuffer === '' ||
+    inputBuffer.slice(-1) === '+' ||
+    inputBuffer.slice(-1) === '-' ||
+    inputBuffer.slice(-1) === '*' ||
+    inputBuffer.slice(-1) === '/'
+  ) {
+    return
+  }
+  try {
+    const percentage = eval(inputBuffer) / 100
+    inputBuffer = percentage.toString()
+    operationDisplay.value = inputBuffer
+    resultDisplay.value = numberWithCommas(inputBuffer)
+  } catch (error) {
+    resultDisplay.value = 'Error'
+    console.log(error)
+    inputBuffer = ''
+    operationDisplay.value = ''
+  }
+}
+
+function numberWithCommas (number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
