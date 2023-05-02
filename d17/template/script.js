1934e1c4fd416168e8213668cb43d42a25da921d
mgckaled <marcelgckaled@gmail.com>
2023-05-02 14:33:42 -0300

    feat(d17/template): create selection logic and change some styles

--- d17/template/script.js
+++ d17/template/script.js
@@ -1,3 +1,4 @@
+// associar os elementos html a variaveis
 const daysTag = document.querySelector('.days')
 const currentDate = document.querySelector('.current-date')
 const prevNextIcon = document.querySelectorAll('#prev, #next')
@@ -23,6 +24,9 @@ const months = [
   'Dezembro'
 ]
 
+// array to store selected dates
+let selectedDates = []
+
 const renderCalendar = () => {
   // associar primeiro dia do mês
   const firstDayofMonth = new Date(currYear, currMonth, 1).getDay()
@@ -41,13 +45,16 @@ const renderCalendar = () => {
   for (let i = 1; i <= lastDateofMonth; i++) {
     // criar um tag <li> para os dias corretes do mês
     // adding active class to li if the current day, month, and year matched
+    const isSelected = selectedDates.some(
+      (date) => date.year === currYear && date.month === currMonth && date.day === i
+    )
     const isToday =
       i === date.getDate() &&
       currMonth === new Date().getMonth() &&
       currYear === new Date().getFullYear()
         ? 'active'
         : ''
-    liTag += `<li class="${isToday}">${i}</li>`
+    liTag += `<li class="${isToday} ${isSelected ? 'selected' : ''}">${i}</li>`
   }
   for (let i = lastDayofMonth; i < 6; i++) {
     // criar uma tag <li> para os primeiros dias do mês seguinte
@@ -55,6 +62,43 @@ const renderCalendar = () => {
   }
   currentDate.innerText = `${months[currMonth]} ${currYear}` // passing current mon and yr as currentDate text
   daysTag.innerHTML = liTag
+
+  // Add click event listener to each date element
+  const dateElements = document.querySelectorAll('.days li')
+  dateElements.forEach((dateEl) => {
+    dateEl.addEventListener('click', () => {
+      const day = parseInt(dateEl.innerText)
+      const isSelected = selectedDates.some(
+        (date) => date.year === currYear && date.month === currMonth && date.day === day
+      )
+      if (isSelected) {
+        selectedDates = selectedDates.filter(
+          (date) => date.year !== currYear || date.month !== currMonth || date.day !== day
+        )
+        dateEl.classList.remove('selected')
+      } else {
+        selectedDates.push({
+          year: currYear,
+          month: currMonth,
+          day
+        })
+        dateEl.classList.add('selected')
+      }
+      displaySelectedDates()
+    })
+  })
+
+  function displaySelectedDates() {
+    const selectedDatesList = document.querySelector('.selected-dates')
+    if (selectedDatesList) {
+      let selectedDatesHTML = ''
+      console.log(selectedDatesHTML)
+      selectedDates.forEach((date) => {
+        selectedDatesHTML += `<li>${date.day}/${date.month}/${date.year}</li>`
+      })
+      selectedDatesList.innerHTML = selectedDatesHTML
+    }
+  }
 }
 
 renderCalendar()
